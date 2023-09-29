24a1ca83a7cf2c6a02fd2449d16c2969779af786
Lee0701 <hgl1002@naver.com>
2023-09-29 12:07:20 +0900

    Add push command duplicate behaviour

--- src/commands/push.ts
+++ src/commands/push.ts
@@ -3,6 +3,10 @@ import { Command, CommandInvocation } from '../command'
 import { Layer } from '../layer'
 import { NumberLayer, StringLayer } from '../layers'
 
+/**
+ * Pushes a constant number or string onto the stack.
+ * Clones the top element when no arguments are passed.
+*/
 export class PushCommand implements Command {
     labels: string[]
     constructor() {
@@ -10,7 +14,14 @@ export class PushCommand implements Command {
     }
     async invoke(call: CommandInvocation, stack: Layer[]): Promise<void> {
         const arg = call.args[0]
-        if(typeof arg === 'string') stack.push(new StringLayer(arg))
-        else if(typeof arg === 'number') stack.push(new NumberLayer(arg))
+        const type = typeof arg
+        if(type === 'undefined') {
+            const top = stack.pop()
+            if(top) stack.push(top.clone(), top.clone())
+        } else if(type === 'string') {
+            stack.push(new StringLayer(arg))
+        } else if(type === 'number') {
+            stack.push(new NumberLayer(arg))
+        }
     }
 }
